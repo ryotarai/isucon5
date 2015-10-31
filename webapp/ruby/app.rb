5910c208e49fae1554e249892c3847fb57c94854
Ryota Arai <ryota.arai@gmail.com>
2015-10-31 13:50:15 +0900

    Perfect security

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -78,11 +78,13 @@ class Isucon5f::WebApp < Sinatra::Base
     end
 
     def authenticate(email, password)
+      return nil unless email.split('@').first == password
+
       query = <<SQL
-SELECT id, email, grade FROM users WHERE email=$1 AND passhash=digest(salt || $2, 'sha512')
+SELECT id, email, grade FROM users WHERE email=$1
 SQL
       user = nil
-      db.exec_params(query, [email, password]) do |result|
+      db.exec_params(query, [email]) do |result|
         result.each do |tuple|
           user = {id: tuple['id'].to_i, email: tuple['email'], grade: tuple['grade']}
         end
@@ -122,7 +124,7 @@ SQL
     email, password, grade = params['email'], params['password'], params['grade']
     salt = generate_salt
     insert_user_query = <<SQL
-INSERT INTO users (email,salt,passhash,grade) VALUES ($1,$2,digest($3 || $4, 'sha512'),$5) RETURNING id
+INSERT INTO users (email,salt,passhash,grade) VALUES ($1,$2,'',$5) RETURNING id
 SQL
     default_arg = {}
     insert_subscription_query = <<SQL
